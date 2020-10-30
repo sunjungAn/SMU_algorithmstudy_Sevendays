5b7e0a02f90a26b5dee4212f13b6f32bbe27873a
sja3410 <55094745+sja3410@users.noreply.github.com>
2020-10-30 11:53:09 +0900

    sort compare + heap_sort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,43 +1,60 @@
 #include<iostream>
-#include<ctime>
 #include<vector>
 #include<string>
+#include<ctime>
+#include<chrono>
 #include<cstdio>
 using namespace std;
+using namespace chrono;
 int temp;
 int insertion_count;
+int heap_count;
 int merge_count;
 int quick_ver01_count;
 int quick_ver02_count;
-int quick_ver03_count = 0;
-static long holdrand = 1L;
+int quick_ver03_count;
+static long hold = 1;
+
 /*
 	학번 : 201910976
 	이름 : 안선정
 	
 	전체적인 소스 구성 :
-		insertion_sort : 삽입정렬을 수행하는 기능과 시간, 비교 횟수를 세고 졍렬된 배열을 출력하는 함수
-		heap_sort : 힙정렬을 수행하는 기능과 시간, 비교 횟수를 세고 정렬된 배열을 출력
-		merge_sort : 합병정렬을 수행하는 기능과 시간, 비교 횟수를 세고 정렬된 배열을 출력
-		quick_sort(ver01, ver02, ver03 ,ver04) : 퀵정렬을 수행하는 기능과 시간, 비교 횟수를 세고 정렬된 배열을 출력
-		main : 1024개의 랜덤 원소를 배정하여 insertion_sort, heap_sort, merge_sort, quick_sort 함수를 수행한다.
-		
+		my_srand, my_rand : 난수를 생성하는 rand함수를 구현한 것, 이 파일의 모든 난수는 이 함수로 배정된다. 
+		insertion_sort : 삽입정렬을 수행하고 비교 횟수를 센다. 
+		heap_sort, heapify : 힙정렬을 수행하고, 비교 횟수를 센다. 
+		merge_sort, merge : 합병정렬을 수행하고, 비교 횟수를 센다. 
+		quick_sort(ver01, ver02, ver03 ,ver04), partition(ver01, ver02, ver03) : 퀵정렬 (ver01, ver02, ver03)을 수행하고, 비교 횟수를 센다. 
+		main : 1024개의 랜덤 원소를 배정하여 insertion_sort, heap_sort, merge_sort, quick_sort 함수를 수행하고, 시간을 측정한다.
+	
+	전역 변수 :
+		temp : 정렬도중 값의 교환이 이루어질때 사용되는 변수이다. 
+		insertion_count : 삽입정렬을 하면서 실행하는 key값의 비교 횟수이다.
+		merge_count : 합병정렬을 하면서 실행하는 key값의 비교 횟수이다.
+		heap_count : 힙정렬을 하면서 실행하는 key값의 비교 횟수이다.
+		quick_ver01_count : 퀵정렬 version1을 하면서 실행하는 key값의 비교 횟수이다.
+		quick_ver02_count : 퀵정렬 version2를 하면서 실행하는 key값의 비교 횟수이다.
+		quick_ver03_count : 퀵정렬 version3을 하면서 실행하는 key값의 비교 횟수이다. 
+
+	코드 진행 방식 : 하나의 배열 arr에 랜덤한 수를 배정하고, 6가지의 정렬을 동일한 배열을 사용하여 정렬하기 위해
+	arr을 미리 설정하고, brr에 arr의 원소를 다 복사하며 각각 정렬을 수행한다. 
 */
-//=====================================랜덤함수====================================================
 
+//===========================================================랜덤함수====================================================
+
+int my_rand()
+{
+	hold =(((hold * hold)+hold) & 0x7fff); //폰노의만의 중앙 제곱법의 의사 난수가 만들어진 방식에 모티브를 얻어서 조금 변형했음
+	return hold; 
+}
 void my_srand(unsigned int seed) {
-	holdrand = (long)seed;
+	hold = (unsigned long)seed;
 }
 
-int my_rand() {
-	return (((holdrand = holdrand * 214013L + 2531011L) >> 16) & 0x7fff);
-}
-
-//====================================삽입정렬========================================================
+//=======================================================삽입정렬========================================================
 void insertion_sort(int arr[], int n)
 {
 	int j, i;
-	clock_t start = clock();
 	for (i = 1; i < n; i++) //1번째 요소는 이미 정렬되어있다고 생각하고 2번째 인자부터 정렬을 시작한다.
 	{
 		temp = arr[i];
@@ -52,41 +69,76 @@ void insertion_sort(int arr[], int n)
 			arr[j + 1] = temp;
 	}
 
-	//printf("insertion sort의 시간: %0.5f\n", (float)(clock() - start) / CLOCKS_PER_SEC);
+}
+//=====================================================힙정렬===========================================================
 
+void heapify(int arr[], int n, int i) {
+	int first = i;
+	int left = 2*i+1; //node의 왼쪽 자식노드
+	int right = 2*i + 2; //오른쪽 자식노드
 
+	if (left < n && arr[left] > arr[first]) {
+		first = left;
+		heap_count++; //키값비교
+	}
+
+	if (right < n && arr[right] > arr[first]) {
+		first = right;
+		heap_count++; //키값비교
+	}
+
+	if (first != i) { 
+		temp = arr[i];
+		arr[i] = arr[first];
+		arr[first] = arr[i];
+		heapify(arr, n, first);
+	}
 }
 
+void heap_sort(int arr[], int n) {
+	
+	for (int i = n / 2 - 1; i >= 0; i--) //max_heap을 구현한다. 
+		heapify(arr, n, i); //max_heap 성질 유지 함수
 
-//==================================합병정렬==============================================
+
+	for (int i = n - 1; i >= 0; i--) {
+		temp = arr[0];
+		arr[0] = arr[i];
+		arr[i] = temp; //root node(max)를 extract하는 과정
+		heapify(arr, i, 0); //max_heap유지
+	}
+}
+
+//====================================================합병정렬===========================================================
 
 void merge(int arr[], int left, int right, int mid) //합병과정
 {
 	int first = left; //앞부분 배열의 첫 인덱스
 	int second = mid + 1; //뒷부분 배열의 첫 인덱스
-	vector <int> v;
+	int copy[1024];
+	int i = 0;
 	
 	while (first <= mid && second <= right) 
 	{
 		merge_count++;
 		if (arr[first] <= arr[second])
-			v.push_back(arr[first++]);
-		else 
-			v.push_back(arr[second++]);
+			copy[i++] = arr[first++];
+		else
+			copy[i++] = arr[second++];
 	}
 	if (first > mid)
 	{
 		while (second <= right)
-			v.push_back(arr[second++]);
+			copy[i++] = arr[second++];
 	}
 	else
 	{
 		while (first <= mid)
-			v.push_back(arr[first++]);
+			copy[i++] = arr[first++];
 	}
-	for (int i = 0; i < v.size(); i++)
+	for (int k = 0; k < i; k++)
 	{
-		arr[i+left] = v[i];
+		arr[k+left] = copy[k];
 	}
 }
 
@@ -102,7 +154,7 @@ void merge_sort(int arr[], int left, int right)
 }
 
 
-//======================퀵 정렬 version1(first or last element as pivot)=======================
+//====================================퀵 정렬 version1(first or last element as pivot)========================================
 
 int partition_ver01(int arr[], int left, int right)
 {
@@ -144,7 +196,7 @@ void quick_sort_ver01(int arr[], int left, int right)
 
 }
 
-//=====================퀵 정렬 version2========================
+//============================================퀵 정렬 version2==============================================================
 int partition_ver02(int arr[], int left, int right)
 {
 	my_srand((unsigned)time(NULL));
@@ -187,13 +239,13 @@ void quick_sort_ver02(int arr[], int left, int right)
 	if (left <= right)
 	{
 		int pivot = partition_ver02(arr, left, right);
-		quick_sort_ver01(arr, left, pivot - 1);
-		quick_sort_ver01(arr, pivot + 1, right);
+		quick_sort_ver02(arr, left, pivot - 1);
+		quick_sort_ver02(arr, pivot + 1, right);
 	}
 }
 
 
-//======================퀵 정렬 version3(median of three partitioning as pivot.)=======================
+//================================퀵 정렬 version3(median of three partitioning as pivot.)=====================================
 
 int partition_ver03(int arr[], int left, int right)
 {
@@ -242,34 +294,84 @@ void quick_sort_ver03(int arr[], int left, int right)
 	if (left <= right)
 	{
 		int pivot = partition_ver03(arr, left, right);
-		quick_sort_ver01(arr, left, pivot - 1);
-		quick_sort_ver01(arr, pivot + 1, right);
+		quick_sort_ver03(arr, left, pivot - 1);
+		quick_sort_ver03(arr, pivot + 1, right);
 	}
 }
-//==================================main==================================================
+//=====================================================main================================================================
 int main(void)
 {
-	int arr[1024];
-	my_srand((unsigned)time(NULL));
-	for (int i = 0; i < 1024; i++)
-	{
-		arr[i] = my_rand()%1000;
-		cout << arr[i] << " ";
-	}
-	int brr[1024], crr[1024], drr[1024], err[1024];
-	memcpy(brr, arr, sizeof(arr));
-	memcpy(drr, arr, sizeof(arr));
-	memcpy(err, arr, sizeof(arr));
-	memcpy(crr, arr, sizeof(arr));
-	insertion_sort(arr, 1024);
-	cout << "insertion_sort의 비교횟수: " << insertion_count;
-	merge_sort(brr, 0, 1023);
-	cout << "merge_sort의 비교횟수: " << merge_count << '\n';
+	while(true){
+		int n;
+		int sort;
+		cout << "배열의 크기  (1024이하, 0입력시 종료): ";
+		cin >> n;
+		if (n == 0) break;
+		int arr[1025];
+		int brr[1025];
+		cout << "sorted? (yes : 1, no : 0):";
+		cin >> sort;
 
-	quick_sort_ver01(crr, 0, 1023);
-	cout << "quicksort_ver01의 비교횟수: " << quick_ver01_count;
-	quick_sort_ver02(drr, 0, 1023);
-	cout << "quicksort_ver02의 비교횟수: " << quick_ver02_count << '\n';
-	quick_sort_ver03(err, 0, 1023);
-	cout << "quicksort_ver03의 비교횟수: " << quick_ver03_count<<'\n';
+		if (sort == 0) {
+			my_srand((unsigned)time(NULL));
+			for (int i = 0; i < n; i++)
+			{
+				arr[i] = my_rand()%1000;
+			}
+		}
+		else {
+			for (int i = 0; i < n; i++)
+				arr[i] = i;
+		}
+		cout << "\t\t비교횟수\t실제시간\n";
+		memcpy(brr, arr, sizeof(arr));
+		system_clock::time_point tp1 = system_clock::now();
+		insertion_sort(brr, n);
+		system_clock::time_point tp2 = system_clock::now();
+		nanoseconds t = tp2 - tp1;
+		printf("insertion sort%10d%15d\n\n", insertion_count, (int)t.count());
+
+		memcpy(brr, arr, sizeof(arr));
+		tp1 = system_clock::now();
+		heap_sort(brr, n);
+		tp2 = system_clock::now();
+		t = tp2 - tp1;
+		printf("heap sort%15d%15d\n\n", heap_count, (int)t.count());
+
+		memcpy(brr, arr, sizeof(arr));
+		tp1 = system_clock::now();
+		merge_sort(brr, 0, n - 1);
+		tp2 = system_clock::now(); t = tp2 - tp1;
+		for (int i = 0; i < n; i++)
+			cout << brr[i] << ' ';
+		printf("merge sort%14d%15d\n\n", merge_count, (int)t.count());
+
+		memcpy(brr, arr, sizeof(arr));
+		tp1 = system_clock::now();
+		quick_sort_ver01(brr, 0, n - 1);
+		tp2 = system_clock::now(); t = tp2 - tp1;
+		printf("quick_ver01%13d%15d\n\n", quick_ver01_count, (int)t.count());
+
+		memcpy(brr, arr, sizeof(arr));
+		tp1 = system_clock::now();
+		quick_sort_ver02(brr, 0, n - 1);
+		tp2 = system_clock::now(); t = tp2 - tp1;
+		printf("quick_ver02%13d%15d\n\n", quick_ver02_count, (int)t.count());
+
+		memcpy(brr, arr, sizeof(arr));
+		tp1 = system_clock::now();
+		quick_sort_ver03(brr, 0, n - 1);
+		tp2 = system_clock::now(); t = tp2 - tp1;
+		printf("quick_ver03%13d%15d\n\n", quick_ver03_count, (int)t.count());
+
+
+		insertion_count = 0;
+		heap_count = 0;
+		merge_count = 0;
+		quick_ver01_count = 0;
+		quick_ver02_count = 0;
+		quick_ver03_count = 0;
+		cout << "===========================================================\n";
+	}
+
 }
