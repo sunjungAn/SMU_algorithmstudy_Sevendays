5a08810945ddf660d48197599319b567ce9d713f
Keastrbs <63184686+Kwondongkyun@users.noreply.github.com>
2021-02-18 17:53:21 +0900

    Create 2438.py

---
+++ Kwondonggyun/2438.py
@@ -0,0 +1,12 @@
+a=int(input())
+for i in range(a):
+    for j in range(0,i+1,1):
+        print('*', end='')
+    print()
+  
+
+
+a=int(input())
+for i in range(1,a+1):
+    print('*'*i, end='\n')
+  
